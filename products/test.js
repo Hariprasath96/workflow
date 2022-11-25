585f3aad5cb2cd5adda3e6ca381e0b595ea86f46
Hari <Hariprasath.V@gds.ey.com>
2022-11-25 10:08:50 +0530

    fix: change

---
+++ products/test.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+
+const jsonsInDir = fs
+  .readdirSync("./")
+  .filter((file) => path.extname(file) === ".json");
+let arr = [];
+
+// const getFileData = () => {
+jsonsInDir.forEach((file) => {
+  const fileData = fs.readFileSync(path.join("./", file));
+  const json = JSON.parse(fileData.toString());
+  arr = [...arr, ...json];
+});
+return arr;
+// };
+
+// getFileData.then((data)=>{
+//     console.log("🚀 ~ file: test.js ~ line 20 ~ jsonsInDir.forEach ~ arr", data)
+//     return data;
+// })
+
+// const data = getFileData();
+// console.log(data)
