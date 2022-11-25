ff87788d4d62b0af45d96e2dcf398e533bd731b0
Hari <Hariprasath.V@gds.ey.com>
2022-11-25 12:15:26 +0530

    fix: simple fix

---
+++ scripts/test.js
@@ -0,0 +1,29 @@
+import fs from "fs-extra";
+import { basename, extname, join } from "path";
+
+const jsonsInDir = fs
+  .readdirSync("../products")
+  .filter((file) => extname(file) === ".json");
+let arr = [];
+
+
+// const getFileData = () => {
+jsonsInDir.forEach((file) => {
+  const fileData = fs.readFileSync(join("../products/", file));
+
+  const json = JSON.parse(fileData.toString());
+  console.log("🚀 ~ file: test.js ~ line 18 ~ jsonsInDir.forEach ~ json", json);
+  arr = [...arr, ...json];
+});
+
+console.log("🚀 ~ file: test.js ~ line 14 ~ jsonsInDir.forEach ~ arr", arr);
+fs.writeFileSync("../products/contents.json", JSON.stringify(arr));
+// };
+
+// getFileData.then((data)=>{
+//     console.log("🚀 ~ file: test.js ~ line 20 ~ jsonsInDir.forEach ~ arr", data)
+//     return data;
+// })
+
+// const data = getFileData();
+// console.log(data)
